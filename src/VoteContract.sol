3efa8eda55afc81871912ca909a8475dbc1d2540
Junion <junionreal@gmail.com>
2023-07-22 08:31:32 -0500

    add small changes

--- src/VoteContract.sol
+++ src/VoteContract.sol
@@ -12,9 +12,12 @@ contract VoteContract is Ownable {
 
     IERC20 public constant NGMI = IERC20(0x123);
 
-    mapping(address => mapping(uint256 => mapping(uint256 => uint256))) public deposits;
-    mapping(uint256 => mapping(uint256 => uint256)) public votes;
-    mapping(uint256 => mapping(uint256 => uint256)) public slashedRatio;
+    mapping(uint256 => mapping(uint256 => VoteSegment)) public VoteSegments;
+    struct VoteSegment {
+        uint256 votes;
+        mapping(address => uint256) deposits;
+        uint256 slashedRatio;
+    }
 
     event Vote(address indexed user, uint256 indexed gauge, uint256 epoch);
 
@@ -24,20 +27,24 @@ contract VoteContract is Ownable {
 
     function deposit(uint256 _gauge, uint256 _amount) external {
         NGMI.transferFrom(msg.sender, address(this), _amount);
-        deposits[msg.sender][_gauge][epoch()] += _amount;
-        votes[_gauge][epoch()] += _amount;
+        VoteSegment storage voteSegment = VoteSegments[epoch()][_gauge];
+        voteSegment.deposits[msg.sender] += _amount;
+        voteSegment.votes += _amount;
         emit Vote(msg.sender, _gauge, epoch());
     }
 
     function withdraw(uint256 _gauge, uint256 _epoch, uint256 _amount) external {
         require(_epoch != epoch() - 1, "tokens locked");
-        deposits[msg.sender][_gauge][_epoch] -= _amount;
-        votes[_gauge][_epoch] -= _amount;
-        NGMI.transfer(msg.sender, _amount * (1e18 - slashedRatio[_gauge][_epoch]) / 1e18);
+        VoteSegment storage voteSegment = VoteSegments[epoch()][_gauge];
+        voteSegment.deposits[msg.sender] -= _amount;
+        voteSegment.votes -= _amount;
+        NGMI.transfer(msg.sender, _amount * (1e18 - voteSegment.slashedRatio) / 1e18);
     }
 
     function coverShortfall(uint256 _gauge, uint256 _amount) external onlyOwner {
+        VoteSegment storage voteSegment = VoteSegments[epoch()][_gauge];
         NGMI.transfer(owner(), _amount);
-        slashedRatio[_gauge][epoch()] += _amount / votes[_gauge][epoch()];
+        voteSegment.slashedRatio += _amount / voteSegment.votes;
+        require(voteSegment.slashedRatio <= 1e18, "overslashed");
     }
 }
